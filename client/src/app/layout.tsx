02262194f19fd147f37af45d6fc01ed4494b6ad8
elitebot47 <elitebot47@gmail.com>
2025-06-30 01:13:03 +0530

    client: sidebar will be closed on mount

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
         <SessionProvider session={session}>
-          <SidebarProvider defaultOpen>
+          <SidebarProvider defaultOpen={false}>
             <div className="flex h-full w-full bg-blue-50">
               <SidebarWrapper />
               <main className="flex-1 flex flex-col ml-0.5 h-screen mr-2">
